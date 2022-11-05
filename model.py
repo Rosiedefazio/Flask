b8b343e7a296dc48f5180d7df3b2d81c757a6a8d
Rosiedefazio <93100217+Rosiedefazio@users.noreply.github.com>
2022-11-04 22:23:35 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -7,6 +7,6 @@ X = df[['Wieght', 'Volume']]
 y = df['CO2']
 
 regr = linear_model.LinearRegression()
-model = regr.fit(X, y)
+model = regr.fit(X.values, y)
 filename = 'finalized_model.pkl'
 pickle.dump(model, open(filename, 'wb'))
